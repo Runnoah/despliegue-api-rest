a50eacddfdf9914362596cd9a13ba5e6bef7ad45
Runoah <rubengarciacubero@gmail.com>
2025-09-22 19:39:51 +0200

    Update

--- src/main/java/com/example/despliegue_api_rest/entity/Pet.java
+++ src/main/java/com/example/despliegue_api_rest/entity/Pet.java
@@ -1,8 +1,53 @@
 package com.example.despliegue_api_rest.entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+
+@Entity
+
 public class Pet {
     
     private Long id;
     private String name;
-    private private private; private;
+    private LocalDate born;
+    private String chip;
+    private String category;
+
+    public Pet() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public LocalDate getBorn() {
+        return born;
+    }
+    public void setBorn(LocalDate born) {
+        this.born = born;
+    }
+    public String getChip() {
+        return chip;
+    }
+    public void setChip(String chip) {
+        this.chip = chip;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    
 }
