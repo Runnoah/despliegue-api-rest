ba3b6ef16f3ace2267a89f511f164e30cbbe4575
Runoah <rubengarciacubero@gmail.com>
2025-09-29 12:46:03 +0200

    Update

--- src/main/java/com/example/despliegue_api_rest/controller/PetController.java
+++ src/main/java/com/example/despliegue_api_rest/controller/PetController.java
@@ -3,11 +3,24 @@ package com.example.despliegue_api_rest.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.despliegue_api_rest.entity.Pet;
+import com.example.despliegue_api_rest.repository.PetRepository;
+
+import java.util.List;
 @RestController
 public class PetController {
     
-    @GetMapping
-    public String helloworld() {
-        return "Hello World";
+    private PetRepository petRepository;
+
+    public PetController (PetRepository petRepository) {
+        this.petRepository = petRepository;
     }
+
+    @GetMapping
+    public List<Pet> hello() {
+
+
+        return petRepository.findAll();
+    }
+
 }
--- src/main/java/com/example/despliegue_api_rest/entity/Pet.java
+++ src/main/java/com/example/despliegue_api_rest/entity/Pet.java
@@ -3,11 +3,15 @@ package com.example.despliegue_api_rest.entity;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 
 public class Pet {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private LocalDate born;
